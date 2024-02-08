f0fda836359761c12d0f24b81a039dc0ea8dea45
JoshuaPirrello <159154079+JoshuaPirrello@users.noreply.github.com>
2024-02-08 09:38:54 -0500

    lab 1 work
    l

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-peter piper picked a patch of purple pickled peppers
+print("Ha Cha Cha \n mama mia ")
+print("pete is big, now he's small")
+print("and this is how im gonna have it all\n let's get dangerous")
+print("i make big things little, and large things small")
