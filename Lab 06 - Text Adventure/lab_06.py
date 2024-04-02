09bdeceb6aff399c0382bf82e8bd7429b640a939
JoshuaPirrello <159154079+JoshuaPirrello@users.noreply.github.com>
2024-04-02 10:11:37 -0400

    completed the hard assignment

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,56 +1,69 @@
-class Cat:
-    def __init__(self):
-        self.color = ""
-        self.name = ""
-        self.weight = 0
+class Room:
+    def __init__(self, description, north, east, south, west):
+         self.description = description
+         self.north = north
+         self.east = east
+         self.south = south
+         self.west = west
 
-    def meow(self):
-        print(self.name + " says  \"Meow")
+
+def main():
+    room_list = []
+
+    room = Room(" A comfortable bedroom full of pillows", 3, 1, None, None)
+    room_list.append(room)
+    room = Room("A gloomy lonely hallway", 4, 2, None, 0)
+    room_list.append(room)
+    room = Room("A living room with a couch and static T.V.", 5, None, None, 1)
+    room_list.append(room)
+    room = Room("A dirty bed surrounded by random items in storage", None, 4, 0, None)
+    room_list.append(room)
+    room = Room("A bright hallway dotted with pictures on the wall", 6, 5, 1, 3)
+    room_list.append(room)
+    room = Room("A kitchen with a sink full of dishes", None, None, 2, 4)
+    room_list.append(room)
+    room = Room("A bathroom! It even has a toilet!", None, None, 4, None)
+    room_list.append(room)
 
 
-my_cat = Cat()
-my_cat.name = "Zero"
-my_cat.color = "black"
-my_cat.weight = 8
 
-my_cat.color = "Black"
-my_cat.name = "Zero"
-my_cat.age = 3
+    current_room = 0
+    next_room = None
 
-another_cat = Cat()
-another_cat.color = "black"
-another_cat.name = "Sniff"
-another_cat.age = 4
+    done = False
 
-my_cat.meow()
-another_cat.meow()
+    while not done:
+        print()
+        print(room_list[current_room].description)
+        print("""
+                A. Go North
+                B. Go East
+                C. Go South
+                D. Go West
+                Q. Quit Game""")
+        print()
+        userInput = input("""Make Your selection now: """)
 
-class Monster:
-    def __init__(self):
-        self.hp = 0
-        self.color = ""
-        self.name = ""
+        if userInput.upper() == "A" or userInput.upper() == "NORTH" or userInput.upper() =="N":
+            next_room = room_list[current_room].north
+        elif userInput.upper() == "B" or userInput.upper() =="EAST" or userInput.upper() =="E":
+            next_room = room_list[current_room].east
+        elif userInput.upper() == "C" or userInput.upper() =="SOUTH" or userInput.upper() == "S":
+            next_room = room_list[current_room].south
+        elif userInput.upper() == "D" or userInput.upper() == "WEST" or userInput.upper() == 'W':
+            next_room = room_list[current_room].west
+        elif userInput.upper() == "Q" or userInput.upper() == "QUIT" or userInput.upper() == "LEAVE":
+            print("game over")
+            break
 
-    def decrease_health(self, damage):
-       self.hp -= damage
-       print(self.name + " has been hit!")
+        if next_room == None:
+                print("You cannot go in this direction, you are blocked by the wall.")
+        else:
+            current_room = next_room
+            print("You walk on ahead")
 
-mush = Monster()
-mush.hp = 5
-mush.color = "Red"
-mush.name = "Ramblin' evil Mushroom"
-print(mush.name + " Has a health of " + str(mush.hp))
+main()
 
-mush.decrease_health(2)
-
-class Star:
-    def __init__(self, name):
-        self.brightness = 0
-        self.age = 0
-        print("A star is born and it is " + name)
-
-star1 = Star("Solaris")
-star2 = Star("Betelgeuse")
 
 
 
