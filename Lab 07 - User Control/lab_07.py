ef5308d1bbb59c1e5adbab8871362b95e5fca1ac
JoshuaPirrello <159154079+JoshuaPirrello@users.noreply.github.com>
2024-04-13 15:49:47 -0400

    Finished lab_07.py
    made it more interesting and gave it a theme

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -7,12 +7,14 @@ class GameWindow(arcade.Window):
         super().__init__(width, height, title)
         self.set_location(400, 200)
 
-        arcade.set_background_color(arcade.color.ANTIQUE_FUCHSIA)
+        arcade.set_background_color(arcade.color.DARK_MIDNIGHT_BLUE)
 
         self.c_x = 100
         self.c_y = 100
         self.x_speed = 300
-        self.y_speed = 150
+        self.y_speed = 300
+        self.red_x = 500
+        self.red_y = 500
 
         self.player_x = 100
         self.player_y = 100
@@ -24,16 +26,32 @@ class GameWindow(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_circle_filled(self.c_x, self.c_y, 50,  arcade.color.PURPLE_PIZZAZZ, 20)
-        arcade.draw_circle_outline(self.player_x, self.player_y, 50, arcade.color.GOLD, 2, 20)
+        screen_width = 600
+        screen_height = 600
+        arcade.draw_lrtb_rectangle_filled(0, screen_height, screen_width / 3, 0, arcade.color.GUPPIE_GREEN)
+        arcade.draw_rectangle_filled(15, 115, 14, 80, arcade.color.REDWOOD)
+        arcade.draw_circle_filled(25, 225, 75, arcade.color.FERN_GREEN)
+        arcade.draw_rectangle_filled(215, 115, 14, 80, arcade.color.REDWOOD)
+        arcade.draw_circle_filled(225, 225, 75, arcade.color.FERN_GREEN)
+        arcade.draw_rectangle_filled(415, 115, 14, 550, arcade.color.REDWOOD)
+        arcade.draw_circle_filled(425, 425, 75, arcade.color.FERN_GREEN)
+        arcade.draw_circle_filled(50, screen_height - 50, 75, arcade.color.SUNSET)
+        arcade.draw_circle_outline(50, screen_height - 50, 75, arcade.color.BLACK_BEAN)
+        arcade.draw_circle_filled(self.c_x, self.c_y, 50, arcade.color.PURPLE_PIZZAZZ, 20)
+        arcade.draw_circle_filled(self.player_x, self.player_y, 70, arcade.color.WHITE)
+        arcade.draw_circle_filled(self.player_x, self.player_y, 50, arcade.color.OCEAN_BOAT_BLUE, 2, 20)
+        arcade.draw_ellipse_filled(self.player_x, self.player_y, screen_width / 20, screen_height / 15, arcade.color.BLACK_OLIVE)
+        arcade.draw_circle_filled(self.red_x, self.red_y, 70, arcade.color.WHITE)
+        arcade.draw_circle_filled(self.red_x, self.red_y, 50, arcade.color.RED_DEVIL, 20)
+        arcade.draw_ellipse_filled(self.red_x, self.red_y, screen_width/20, screen_height/15, arcade.color.BLACK_OLIVE)
 
-    def update(self, delta_time):
+    def on_update(self, delta_time):
         self.c_x += self.x_speed * delta_time
         self.c_y += self.y_speed * delta_time
 
-        if self.c_x > 1280 - 50 or self.c_x < 0 + 50:
+        if self.c_x > 600 - 50 or self.c_x < 0 + 50:
             self.x_speed *= -1
-        if self.c_y > 720 - 50 or self.c_y < 0 + 50:
+        if self.c_y > 600 - 50 or self.c_y < 0 + 50:
             self.y_speed *= -1
 
         if self.right:
@@ -65,6 +83,18 @@ class GameWindow(arcade.Window):
         if symbol == arcade.key.DOWN:
             self.down = False
 
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.red_x = x
+            self.red_y = y
+        if button == arcade.MOUSE_BUTTON_RIGHT:
+            self.red_x = x
+            self.red_y = y
 
-GameWindow(1280, 720, "Basic Bounce")
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        self.red_x = x
+        self.red_y = y
+
+
+GameWindow(600, 600, "Witch's Eyes over spooky forest ")
 arcade.run()
